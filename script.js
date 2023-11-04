9edf548f2e0aee7c42178e809e11415c4924918d
Muhammad Naufal <irsanfarrel@gmail.com>
2023-11-04 12:46:19 +0700

    menambahkan beberapa style baru

--- script.js
+++ script.js
@@ -2,20 +2,35 @@ let greyBtn = document.getElementById("grey");
 let whiteBtn = document.getElementById("white");
 let orangeBtn = document.getElementById("orange");
 let greenBtn = document.getElementById("green");
-let bike = document.getElementById("bike");
+let bike = document.getElementById("vespa");
 
 greyBtn.onclick = function() {
-    bike.style.backgroundImage = "url(vespa-img/blackvespa.png)";
+    bike.style.backgroundImage = "url(assets/images/blackvespa.png)";
 }
 
 whiteBtn.onclick = function() {
-    bike.style.backgroundImage = "url(vespa-img/whitevespa.png)";
+    bike.style.backgroundImage = "url(assets/images/whitevespa.png)";
 }
 
 orangeBtn.onclick = function() {
-    bike.style.backgroundImage = "url(vespa-img/orangevespa.png)";
+    bike.style.backgroundImage = "url(assets/images/orangevespa.png)";
 }
 
 greenBtn.onclick = function() {
-    bike.style.backgroundImage = "url(vespa-img/greenvespa.png)";
+    bike.style.backgroundImage = "url(assets/images/greenvespa.png)";
+}
+
+// function scroll
+function handleClick(param) {
+    if (param === "home") {
+       return location.href = "#home"
+    } else if (param === "about") {
+       return location.href = "#about"
+    } else if (param === "services") {
+       return location.href = "#services"
+    } else if (param === "contact") {
+       return location.href = "#contact"
+    } else {
+
+    }
 }
