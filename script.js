eeec8d02795f43cad2558b8fa51e7c449a5ebf7d
Muhammad Naufal <irsanfarrel@gmail.com>
2023-11-04 21:23:59 +0700

    menambahkan beberapa function

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let whiteBtn = document.getElementById("white");
 let orangeBtn = document.getElementById("orange");
 let greenBtn = document.getElementById("green");
 let bike = document.getElementById("vespa");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar-nav")
 
 greyBtn.onclick = function() {
     bike.style.backgroundImage = "url(assets/images/blackvespa.png)";
@@ -33,4 +35,9 @@ function handleClick(param) {
     } else {
 
     }
+}
+
+menu.onclick = () => {
+    menu.classList.toggle('ph-x');
+    navbar.classList.toggle('open');
 }
